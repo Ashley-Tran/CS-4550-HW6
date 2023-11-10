ef5a7b32094ea2427cfb5d5609fc4d555ac44f27
Ashley Tran <73571838+Ashley-Tran@users.noreply.github.com>
2023-11-09 22:14:48 -0800

    Functional Program
    Added an external image texture mapping & reused the old program for shape and rotatability.

--- HW6A.js
+++ HW6A.js
@@ -1,211 +1,189 @@
 "use strict";
 
-var shadedCube = function () {
-  var canvas;
-  var gl;
+var canvas;
+var gl;
 
-  var numPositions = 36;
+var numPositions = 36;
+var texSize = 64;
+var program;
+var flag = true;
+var positionsArray = [];
+var colorsArray = [];
+var texCoordsArray = [];
+var texture;
+var texCoord = [vec2(0, 0), vec2(0, 1), vec2(1, 1), vec2(1, 0)];
 
-  var positionsArray = [];
-  var normalsArray = [];
+var vertices = [
+  vec4(-0.5, -0.15, 0.5, 1.0),
+  vec4(-0.1, 0.3, 0.24, 1.0),
+  vec4(0.5, 0.5, 0.3, 1.0),
+  vec4(0.25, -0.38, 0.1, 1.0),
+  vec4(-0.5, -0.3, -0.18, 1.0),
+  vec4(-0.3, 0.5, -0.45, 1.0),
+  vec4(0.24, 0.15, -0.3, 1.0),
+  vec4(0.3, -0.35, -0.5, 1.0),
+];
 
-  var vertices = [
-    vec4(-0.5, -0.15, 0.5, 1.0),
-    vec4(-0.1, 0.3, 0.24, 1.0),
-    vec4(0.5, 0.5, 0.3, 1.0),
-    vec4(0.25, -0.38, 0.1, 1.0),
-    vec4(-0.5, -0.3, -0.18, 1.0),
-    vec4(-0.3, 0.5, -0.45, 1.0),
-    vec4(0.24, 0.15, -0.3, 1.0),
-    vec4(0.3, -0.35, -0.5, 1.0),
-  ];
+var vertexColors = [
+  vec4(0.0, 0.0, 0.0, 1.0), // black
+  vec4(1.0, 0.0, 0.0, 1.0), // red
+  vec4(1.0, 1.0, 0.0, 1.0), // yellow
+  vec4(0.0, 1.0, 0.0, 1.0), // green
+  vec4(0.0, 0.0, 1.0, 1.0), // blue
+  vec4(1.0, 0.0, 1.0, 1.0), // magenta
+  vec4(0.0, 1.0, 1.0, 1.0), // white
+  vec4(0.0, 1.0, 1.0, 1.0), // cyan
+];
 
-  var lightPosition = vec4(1.0, 1.0, 1.0, 0.0);
-  var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
-  var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
-  var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+var axis = xAxis;
+var isNegative = false;
+var theta = vec3(45.0, 45.0, 45.0);
 
-  var materialAmbient = vec4(1.0, 0.0, 1.0, 1.0);
-  var materialDiffuse = vec4(0.447, 0.067, 0.941, 1.0);
-  var materialSpecular = vec4(0.247, 0.167, 0.641, 1.0);
-  var materialShininess = 100.0;
+var thetaLoc;
 
-  var ctm;
-  var ambientColor, diffuseColor, specularColor;
-  var modelViewMatrix, projectionMatrix;
-  var viewerPos;
-  var program;
+function configureTexture(image) {
+  texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  gl.generateMipmap(gl.TEXTURE_2D);
+  gl.texParameteri(
+    gl.TEXTURE_2D,
+    gl.TEXTURE_MIN_FILTER,
+    gl.NEAREST_MIPMAP_LINEAR
+  );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
-  var xAxis = 0;
-  var yAxis = 1;
-  var zAxis = 2;
-  var isNegative = false;
-  var axis = 0;
-  var theta = vec3(0, 0, 0);
+  gl.uniform1i(gl.getUniformLocation(program, "uTexMap"), 0);
+}
 
-  var thetaLoc;
+function quad(a, b, c, d) {
+  positionsArray.push(vertices[a]);
+  colorsArray.push(vertexColors[a]);
+  texCoordsArray.push(texCoord[0]);
 
-  var flag = false;
+  positionsArray.push(vertices[b]);
+  colorsArray.push(vertexColors[a]);
+  texCoordsArray.push(texCoord[1]);
 
-  function quad(a, b, c, d) {
-    var t1 = subtract(vertices[b], vertices[a]);
-    var t2 = subtract(vertices[c], vertices[b]);
-    var normal = cross(t1, t2);
-    normal = vec3(normal);
+  positionsArray.push(vertices[c]);
+  colorsArray.push(vertexColors[a]);
+  texCoordsArray.push(texCoord[2]);
 
-    positionsArray.push(vertices[a]);
-    normalsArray.push(normal);
-    positionsArray.push(vertices[b]);
-    normalsArray.push(normal);
-    positionsArray.push(vertices[c]);
-    normalsArray.push(normal);
-    positionsArray.push(vertices[a]);
-    normalsArray.push(normal);
-    positionsArray.push(vertices[c]);
-    normalsArray.push(normal);
-    positionsArray.push(vertices[d]);
-    normalsArray.push(normal);
-  }
+  positionsArray.push(vertices[a]);
+  colorsArray.push(vertexColors[a]);
+  texCoordsArray.push(texCoord[0]);
 
-  function colorCube() {
-    quad(1, 0, 3, 2);
-    quad(2, 3, 7, 6);
-    quad(3, 0, 4, 7);
-    quad(6, 5, 1, 2);
-    quad(4, 5, 6, 7);
-    quad(5, 4, 0, 1);
-  }
+  positionsArray.push(vertices[c]);
+  colorsArray.push(vertexColors[a]);
+  texCoordsArray.push(texCoord[2]);
 
-  window.onload = function init() {
-    canvas = document.getElementById("gl-canvas");
-    gl = canvas.getContext("webgl2");
-    if (!gl) alert("WebGL 2.0 isn't available");
+  positionsArray.push(vertices[d]);
+  colorsArray.push(vertexColors[a]);
+  texCoordsArray.push(texCoord[3]);
+}
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+function colorCube() {
+  quad(1, 0, 3, 2);
+  quad(2, 3, 7, 6);
+  quad(3, 0, 4, 7);
+  quad(6, 5, 1, 2);
+  quad(4, 5, 6, 7);
+  quad(5, 4, 0, 1);
+}
 
-    gl.enable(gl.DEPTH_TEST);
+window.onload = function init() {
+  canvas = document.getElementById("gl-canvas");
 
-    //  Load shaders and initialize attribute buffers
-    program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
+  gl = canvas.getContext("webgl2");
+  if (!gl) alert("WebGL 2.0 isn't available");
 
-    colorCube();
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    var nBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+  gl.enable(gl.DEPTH_TEST);
 
-    var normalLoc = gl.getAttribLocation(program, "aNormal");
-    gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(normalLoc);
+  //  Load shaders and initialize attribute buffers
+  program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
 
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
+  colorCube();
 
-    var positionLoc = gl.getAttribLocation(program, "aPosition");
-    gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(positionLoc);
+  var cBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
 
-    thetaLoc = gl.getUniformLocation(program, "theta");
+  var colorLoc = gl.getAttribLocation(program, "aColor");
+  gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(colorLoc);
 
-    viewerPos = vec3(0.0, 0.0, -20.0);
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
 
-    projectionMatrix = ortho(-1, 1, -1, 1, -100, 100);
+  var positionLoc = gl.getAttribLocation(program, "aPosition");
+  gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(positionLoc);
 
-    var ambientProduct = mult(lightAmbient, materialAmbient);
-    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
-    var specularProduct = mult(lightSpecular, materialSpecular);
+  var tBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
 
-    window.addEventListener("keydown", function (event) {
-      switch (event.key) {
-        case "ArrowRight":
-          axis = yAxis;
-          isNegative = false;
-          break;
-        case "ArrowLeft":
-          axis = yAxis;
-          isNegative = true;
-          break;
-        case "ArrowUp":
-          axis = xAxis;
-          isNegative = true;
-          break;
-        case "ArrowDown":
-          axis = xAxis;
-          isNegative = false;
-          break;
-      }
-      if(event.key == "ArrowRight" || event.key == "ArrowLeft" || event.key == "ArrowUp" || event.key == "ArrowDown"){
-        flag = !flag;
-      }
-    });
+  var texCoordLoc = gl.getAttribLocation(program, "aTexCoord");
+  gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(texCoordLoc);
 
-    gl.uniform4fv(
-      gl.getUniformLocation(program, "uAmbientProduct"),
-      ambientProduct
-    );
-    gl.uniform4fv(
-      gl.getUniformLocation(program, "uDiffuseProduct"),
-      diffuseProduct
-    );
-    gl.uniform4fv(
-      gl.getUniformLocation(program, "uSpecularProduct"),
-      specularProduct
-    );
-    gl.uniform4fv(
-      gl.getUniformLocation(program, "uLightPosition"),
-      lightPosition
-    );
+  // Initialize a texture
+  var image = document.getElementById("texImage");
 
-    gl.uniform1f(
-      gl.getUniformLocation(program, "uShininess"),
-      materialShininess
-    );
+  configureTexture(image);
 
-    gl.uniformMatrix4fv(
-      gl.getUniformLocation(program, "uProjectionMatrix"),
-      false,
-      flatten(projectionMatrix)
-    );
-    render();
-  };
+  thetaLoc = gl.getUniformLocation(program, "uTheta");
 
-  var render = function () {
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-gl.clearColor(0.0, 0.0, 0.0, 1);
-    if (flag) {
-      if (isNegative) {
-        theta[axis] -= 2.0;
-      } else {
-        theta[axis] += 2.0;
-      }
+  window.addEventListener("keydown", function (event) {
+    switch (event.key) {
+      case "ArrowRight":
+        axis = yAxis;
+        isNegative = false;
+        break;
+      case "ArrowLeft":
+        axis = yAxis;
+        isNegative = true;
+        break;
+      case "ArrowUp":
+        axis = xAxis;
+        isNegative = true;
+        break;
+      case "ArrowDown":
+        axis = xAxis;
+        isNegative = false;
+        break;
     }
-    modelViewMatrix = mat4();
-    modelViewMatrix = mult(
-      modelViewMatrix,
-      rotate(theta[xAxis], vec3(1, 0, 0))
-    );
-    modelViewMatrix = mult(
-      modelViewMatrix,
-      rotate(theta[yAxis], vec3(0, 1, 0))
-    );
-    modelViewMatrix = mult(
-      modelViewMatrix,
-      rotate(theta[zAxis], vec3(0, 0, 1))
-    );
-
-    gl.uniformMatrix4fv(
-      gl.getUniformLocation(program, "uModelViewMatrix"),
-      false,
-      flatten(modelViewMatrix)
-    );
-
-    gl.drawArrays(gl.TRIANGLES, 0, numPositions);
-
-    requestAnimationFrame(render);
-  };
+    if (
+      event.key == "ArrowRight" ||
+      event.key == "ArrowLeft" ||
+      event.key == "ArrowUp" ||
+      event.key == "ArrowDown"
+    ) {
+      flag = !flag;
+    }
+  });
+  render();
 };
 
-shadedCube();
+var render = function () {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.clearColor(0.0, 0.0, 0.0, .3);
+  if (flag) {
+    if (isNegative) {
+      theta[axis] -= 2.0;
+    } else {
+      theta[axis] += 2.0;
+    }
+  }
+  gl.uniform3fv(thetaLoc, theta);
+  gl.drawArrays(gl.TRIANGLES, 0, numPositions);
+  requestAnimationFrame(render);
+};
